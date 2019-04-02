4f8c7da82b7578f98f8892cfd21037447951418d
yokobond <koji.yokokawa@gmail.com>
2019-04-03 08:58:39 +0900

    Build for 021063470da0a1ff8f8b24bc846e446ad26bd457

---
+++ src/components/context-menu/context-menu.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {ContextMenu, MenuItem} from 'react-contextmenu';
+import classNames from 'classnames';
+
+import styles from './context-menu.css';
+
+const StyledContextMenu = props => (
+    <ContextMenu
+        {...props}
+        className={styles.contextMenu}
+    />
+);
+
+const StyledMenuItem = props => (
+    <MenuItem
+        {...props}
+        attributes={{className: styles.menuItem}}
+    />
+);
+
+const BorderedMenuItem = props => (
+    <MenuItem
+        {...props}
+        attributes={{className: classNames(styles.menuItem, styles.menuItemBordered)}}
+    />
+);
+
+
+export {
+    BorderedMenuItem,
+    StyledContextMenu as ContextMenu,
+    StyledMenuItem as MenuItem
+};
