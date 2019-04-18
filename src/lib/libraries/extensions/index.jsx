0f18cabb4f24dc796b75cc927e06405a4e252c4e
yokobond <koji.yokokawa@gmail.com>
2019-04-18 18:30:29 +0900

    Make microbit-more as a new extension.

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -43,7 +43,42 @@ import gdxforInsetIconURL from './gdxfor/gdxfor-small.svg';
 import gdxforConnectionIconURL from './gdxfor/gdxfor-illustration.svg';
 import gdxforConnectionSmallIconURL from './gdxfor/gdxfor-small.svg';
 
+import microbitMoreIconURL from './microbitMore/microbitMore.png';
+import microbitMoreInsetIconURL from './microbitMore/microbitMore-small.svg';
+import microbitMoreConnectionIconURL from './microbitMore/microbitMore-illustration.svg';
+import microbitMoreConnectionSmallIconURL from './microbitMore/microbitMore-small.svg';
+
 export default [
+    {
+        name: 'micro:bit More',
+        extensionId: 'microbitMore',
+        collaborator: 'Yengawa Lab',
+        iconURL: microbitMoreIconURL,
+        insetIconURL: microbitMoreInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Connect your projects with the world."
+                description="Description for the 'micro:bit' extension"
+                id="gui.extension.microbit.description"
+            />
+        ),
+        featured: true,
+        disabled: false,
+        bluetoothRequired: true,
+        internetConnectionRequired: true,
+        launchPeripheralConnectionFlow: true,
+        useAutoScan: false,
+        connectionIconURL: microbitMoreConnectionIconURL,
+        connectionSmallIconURL: microbitMoreConnectionSmallIconURL,
+        connectingMessage: (
+            <FormattedMessage
+                defaultMessage="Connecting"
+                description="Message to help people connect to their micro:bit."
+                id="gui.extension.microbit.connectingMessage"
+            />
+        ),
+        helpLink: 'https://scratch.mit.edu/microbit'
+    },
     {
         name: (
             <FormattedMessage
