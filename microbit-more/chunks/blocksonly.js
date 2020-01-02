afa9e66b02da73f6e450c66932e54cb6d9f6abe3
yokobond <kobo.yokokawa@gmail.com>
2020-01-02 13:33:37 +0900

    Build for $(git log --pretty=format:%H -n1)

--- microbit-more/chunks/blocksonly.js
+++ microbit-more/chunks/blocksonly.js
@@ -13,11 +13,11 @@ exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader
 
 
 // module
-exports.push([module.i, ".blocks-only_controls_2GEvs {\n    position: absolute;\n    z-index: 2;\n    top: 10px;\n    right: 15px;\n}\n", ""]);
+exports.push([module.i, ".blocks-only_controls_1Fuds {\r\n    position: absolute;\r\n    z-index: 2;\r\n    top: 10px;\r\n    right: 15px;\r\n}\r\n", ""]);
 
 // exports
 exports.locals = {
-	"controls": "blocks-only_controls_2GEvs"
+	"controls": "blocks-only_controls_1Fuds"
 };
 
 /***/ }),
